0991e9e80f5d5e3f160ae61ee81925df2ee5ccd1
groganj8 <jake.grogan8@mail.dcu.ie>
2020-03-28 00:17:02 +0000

    inti

---
+++ deck.py
@@ -0,0 +1,59 @@
+class Deck(object):
+    def __init__(self):
+        self.cards = []
+        self.build()
+
+    # Display all cards in the deck
+    def show(self):
+        for card in self.cards:
+            print(card.show())
+
+    def pullCard(self):
+        return self.cards.pop()
+
+    # Generate 52 cards
+    def build(self):
+        self.cards = []
+        for suit in ['Hearts', 'Clubs', 'Diamonds', 'Spades']:
+            for val in range(1,14):
+                self.cards.append(Card(suit, val))
+
+    # Shuffle the deck
+    def shuffle(self, num=1):
+        length = len(self.cards)
+        for _ in range(num):
+            # This is the fisher yates shuffle algorithm
+            for i in range(length-1, 0, -1):
+                randi = random.randint(0, i)
+                if i == randi:
+                    continue
+                self.cards[i], self.cards[randi] = self.cards[randi], self.cards[i]
+            # You can also use the build in shuffle method
+            # random.shuffle(self.cards)
+
+class Card(object):
+    def __init__(self, suit, val):
+        self.suit = suit
+        self.value = val
+
+    # Implementing build in methods so that you can print a card object
+    def __unicode__(self):
+        return self.show()
+    def __str__(self):
+        return self.show()
+    def __repr__(self):
+        return self.show()
+        
+    def show(self):
+        if self.value == 1:
+            val = "Ace"
+        elif self.value == 11:
+            val = "Jack"
+        elif self.value == 12:
+            val = "Queen"
+        elif self.value == 13:
+            val = "King"
+        else:
+            val = self.value
+
+        return "{} of {}".format(val, self.suit)
---
+++ server.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import json
+
+from deck import Deck, Card
+
+app = Flask(__name__)
+
+socketio = SocketIO(app)
+
+connection_count = 0
+
+deck = Deck()
+
+players = ["Jack", "Jake", "Alina", "Greta", "Cormac", "Eoghan", "Sophie", "Aaisha", "Sean", "Leah", "Carrie"]
+global i
+i = 0
+
+@socketio.on('onconnect')
+def handle_onconnect(message):
+	global connection_count
+	connection_count += 1
+	print(message)
+
+@socketio.on('pullcard')
+def handle_skip():
+    global i
+    card = deck.pullCard()
+    print(card)
+    i += 1
+    socketio.emit('response', {"card": card.__str__(), "player": players[i]})
+    if i == len(players) - 1:
+        i = 0
+    return 
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/game')
+def game():
+	if connection_count >= 15:
+		print("REFUSED")
+		return "error.html"
+	return render_template("game.html")
+
+if __name__ == '__main__':
+    # Encapsulates the start up of the web server
+    socketio.run(app, port="5000", host="0.0.0.0")
